6acb8713b35e198b73f2920b3b827c029fdd2a58
Bill Hofmann <bill.hofmann@gmail.com>
2013-07-22 16:09:37 -0700

    Update team model

--- Team.rb
+++ Team.rb
@@ -1,19 +1,23 @@
 # Team.rb
 
-require 'data_mapper'
-
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+require 'mongoid'
 
 class Team
-    include DataMapper::Resource
-    property :id, Serial
-    property :name, String
-    property :created_at, DateTime
+    include Mongoid::Document
+    field :owner						# a member
+    field :name, type: String			# display name
+    field :description, type: String	# description
+    field :location, type: String		# city/etc.  Can be overridden at the member level
+    field :timezone, type: String		# timezone info.  Can be overridden at the member level
+    # need working/core hours here
+    embeds_many :members				# team member
 end
 
-# Perform basic sanity checks and initialize all relationships
-# Call this when you've defined all your models
-DataMapper.finalize
-
-# automatically create the table
-Team.auto_upgrade!
+class Member
+	include Mongoid::Document
+	field :name, type: String
+	field :email, type: String
+	field :location, type: String		# if empty, defaults to team
+	field :timezone, type: String		# if empty, defaults to team
+	embedded_in :team
+end
